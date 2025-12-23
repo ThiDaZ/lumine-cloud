24998d93e55b4e4eb70a707cae1bece51a89bd72
ThiDaZ <wthidas95@gmail.com>
2025-12-23 21:42:51 +0530

    added integration test for product creation and updated .gitignore

--- product-service/src/test/java/com/lumine/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/lumine/product/ProductServiceApplicationTests.java
@@ -1,10 +1,25 @@
 package com.lumine.product;
 
+import com.lumine.product.dto.ProductRequest;
+import com.lumine.product.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
 
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Testcontainers
@@ -15,5 +30,40 @@ class ProductServiceApplicationTests {
     void contextLoads() {
     }
 
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0");
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @Test
+    void shouldCreateProduct() throws Exception{
+
+        ProductRequest productRequest = getProductRequest();
+        String productRequestString = objectMapper.writeValueAsString(productRequest);
+
+        // Act (Call the endpoint)
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(productRequestString))
+                .andExpect(status().isCreated()); // Assert 201 Created
+
+        Assertions.assertEquals(1, productRepository.findAll().size());
+    }
+
+    private ProductRequest getProductRequest(){
+        return new ProductRequest("iPhone 15", "Apple Smartphone", BigDecimal.valueOf(1200));
+    }
 
 }
